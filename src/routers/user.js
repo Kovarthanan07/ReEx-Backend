da4570933a4073e970b20388bc0b9a81f797d58e
Kovarthanan07 <kovarthanan07@gmail.com>
2021-02-15 01:29:05 +0530

    first commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
-
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -13,15 +13,77 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
+
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = [
+    "name",
+    "email",
+    "password",
+    "dateOfBirth",
+    "mobileNumber",
+    "gender",
+  ];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 router.post("/users/login", async (req, res) => {
   try {
     const user = await User.findByCredentials(
       req.body.userId,
       req.body.password
     );
-    // const token = await user.generateAuthToken();
-    // res.send({ user, token });
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
